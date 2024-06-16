8286e6c1fffde8878d5da6ddce3fd6af61a170aa
same06 <typewreter@yahoo.co.jp>
2024-06-16 10:48:58 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,9 +12,11 @@ class OrdersController < ApplicationController
   def create
     @order = current_user.orders.build(order_params)
     
-    @order.with_lock do
-      @order.save!
-      @order.update_total_quantity
+    ActiveRecord::Base.transaction do
+      @order.with_lock do
+        @order.save!
+        @order.update_total_quantity
+      end
     end
     # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
     redirect_to orders_path
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,7 @@ class Order < ApplicationRecord
   accepts_nested_attributes_for :ordered_lists
 
   def update_total_quantity
+    #ActiveRecord::Base.transaction do
     self.ordered_lists.each do |line_item|
       item = Item.lock.find_by(id: line_item.item_id)
       item.total_quantity += line_item.quantity
