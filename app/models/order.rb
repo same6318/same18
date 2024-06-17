4cadbd855119d1c29c9d2cd76012219c7aeb397f
same06 <typewreter@yahoo.co.jp>
2024-06-17 14:25:45 +0900

    update

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,9 +7,11 @@ class Order < ApplicationRecord
   def update_total_quantity
     #ActiveRecord::Base.transaction do
     self.ordered_lists.each do |line_item|
-      item = Item.lock.find_by(id: line_item.item_id)
-      item.total_quantity += line_item.quantity
-      item.save!
+      item = Item.find_by(id: line_item.item_id)
+      item.with_lock do
+        item.total_quantity += line_item.quantity
+        item.save!
+      end
     end
   end
 end
